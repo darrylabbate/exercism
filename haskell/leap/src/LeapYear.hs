9bd59fb4a6c43120ad93eb603997a71d6898d710
Darryl Abbate <rootbeersoup@gmail.com>
2018-07-14 01:06:27 -0500

    Update LeapYear.hs

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,9 +1,8 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year
-  | isDivBy 400 = True
-  | isDivBy 100 = False
-  | isDivBy 4   = True
-  | otherwise   = False
-  where isDivBy x = mod year x == 0
+isLeapYear year | isDivBy 400 = True
+                | isDivBy 100 = False
+                | isDivBy 4   = True
+                | otherwise   = False
+                where isDivBy x = rem year x == 0
