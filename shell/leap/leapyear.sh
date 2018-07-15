3e766e93b33cade3305e00741ac6af032043ed23
Darryl Abbate <rootbeersoup@gmail.com>
2018-07-15 12:39:57 -0500

    Add leapyear.sh

--- shell/leap/leapyear.sh
+++ shell/leap/leapyear.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+year=$1
+
+if [ $((year % 400)) -eq 0 ]; then
+  echo "True"
+elif [ $((year % 100)) -eq 0 ]; then
+  echo "False"
+elif [ $((year % 4)) -eq 0 ]; then
+  echo "True"
+else
+  echo "False"
+fi
