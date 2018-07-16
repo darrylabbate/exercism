0a11e88d61eef7172921e0787e6548ab67fe5c8e
Darryl Abbate <rootbeersoup@gmail.com>
2018-07-16 16:09:52 -0500

    Add bob

---
+++ haskell/bob/src/Bob.hs
@@ -0,0 +1,28 @@
+module Bob (responseFor) where
+
+import           Data.Char
+
+data Address = Question
+             | Yell
+             | YellQuestion
+             | Silence
+             | Normal
+
+classify :: String -> Address
+classify a | all isSpace a = Silence
+           | yell && question = YellQuestion
+           | yell = Yell
+           | question = Question
+           | otherwise = Normal
+           where yell = any isAlpha a && all isUpper (filter isAlpha a)
+                 question = '?' == last (filter (not . isSpace) a)
+
+response :: Address -> String
+response Silence      = "Fine. Be that way!"
+response YellQuestion = "Calm down, I know what I'm doing!"
+response Yell         = "Whoa, chill out!"
+response Question     = "Sure."
+response Normal       = "Whatever."
+
+responseFor :: String -> String
+responseFor = response . classify
