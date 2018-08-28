b9e6a3e44d98c5ed911e669d5ed534e5f1d5d874
Darryl Abbate <rootbeersoup@gmail.com>
2018-08-28 14:11:01 -0500

    Add RNA-transcription

---
+++ haskell/rna-transcription/src/DNA.hs
@@ -0,0 +1,11 @@
+module DNA (toRNA) where
+
+import qualified Data.Map.Strict as Map
+
+toRNA :: String -> Maybe String
+toRNA = mapM (`Map.lookup` rna)
+  where
+    rna = Map.fromList [ ('C', 'G')
+                       , ('G', 'C')
+                       , ('A', 'U')
+                       , ('T', 'A') ]
