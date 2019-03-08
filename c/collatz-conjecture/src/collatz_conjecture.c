17c19c8a00f477a3e6df3e9386bbef06333e1f02
Darryl Abbate <rootbeersoup@gmail.com>
2019-03-07 19:39:39 -0600

    Update solution

--- c/collatz-conjecture/src/collatz_conjecture.c
+++ c/collatz-conjecture/src/collatz_conjecture.c
@@ -8,6 +8,5 @@ int steps(int start)
 int collatz(int step, int n)
 {
   if (n == 1) return step;
-  n = (n & 1) ? n * 3 + 1 : n / 2;
-  return collatz(step + 1, n);
+  return (n & 1) ? collatz(step+1, n*3+1) : collatz(step+1, n/2);
 }
