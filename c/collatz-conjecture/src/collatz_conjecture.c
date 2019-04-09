22ee2641bd6204f9cd39078e1fe643d6afe0bff2
Darryl Abbate <rootbeersoup@gmail.com>
2019-04-09 12:56:55 -0500

    Add spacing around infix operators

--- c/collatz-conjecture/src/collatz_conjecture.c
+++ c/collatz-conjecture/src/collatz_conjecture.c
@@ -3,7 +3,7 @@
 static int collatz(int step, int n)
 {
   if (n == 1) return step;
-  return collatz(step+1, (n & 1) ? n*3+1 : n/2);
+  return collatz(step + 1, (n & 1) ? n * 3 + 1 : n / 2);
 }
 
 int steps(int start)
