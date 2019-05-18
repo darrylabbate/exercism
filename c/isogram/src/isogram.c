b69a52631dacf63257e5582997a94785e66d6874
Darryl Abbate <rootbeersoup@gmail.com>
2019-05-18 14:33:40 -0500

    [c] Add isogram

---
+++ c/isogram/src/isogram.c
@@ -0,0 +1,18 @@
+#include "isogram.h"
+#include <ctype.h>
+#include <stddef.h>
+#include <stdint.h>
+
+static bool eval(int i, uint32_t u, const char phrase[])
+{
+  if (isalpha(phrase[i])) {
+    uint32_t m = 1 << (toupper(phrase[i]) - 'A');
+    return u & m ? false : eval(i + 1, u |= m, phrase);
+  }
+  return phrase[i] == '\0' ? true : eval(i + 1, u, phrase);
+}
+
+bool is_isogram(const char phrase[])
+{
+  return phrase == NULL ? false : eval(0, 0, phrase);
+}
