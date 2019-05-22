fabd6e0256cd0837b315f210fe880064958c69d8
Darryl Abbate <rootbeersoup@gmail.com>
2019-05-22 00:00:10 -0500

    [c/collatz] Tweak style

--- c/collatz-conjecture/src/collatz_conjecture.c
+++ c/collatz-conjecture/src/collatz_conjecture.c
@@ -2,11 +2,10 @@
 
 static int collatz(int step, int n)
 {
-  if (n == 1) return step;
-  return collatz(step + 1, (n & 1) ? n * 3 + 1 : n / 2);
+  return n == 1 ? step : collatz(step + 1, (n & 1) ? n * 3 + 1 : n / 2);
 }
 
 int steps(int start)
 {
-  return (start <= 0) ? ERROR_VALUE : collatz(0, start);
+  return start <= 0 ? ERROR_VALUE : collatz(0, start);
 }
