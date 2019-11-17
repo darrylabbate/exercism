f6ac2032923e3e8fba751a0b30644a726a44c7b7
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-17 17:06:29 -0600

    [c/collatz] Update style

--- c/collatz-conjecture/src/collatz_conjecture.c
+++ c/collatz-conjecture/src/collatz_conjecture.c
@@ -1,11 +1,9 @@
 #include "collatz_conjecture.h"
 
-static int collatz(int step, int n)
-{
-  return n == 1 ? step : collatz(step + 1, (n & 1) ? n * 3 + 1 : n / 2);
+static int collatz(int step, int n) {
+    return n == 1 ? step : collatz(++step, (n&1) ? n*3+1 : n/2);
 }
 
-int steps(int start)
-{
-  return start <= 0 ? ERROR_VALUE : collatz(0, start);
+int steps(int start) {
+    return start <= 0 ? ERROR_VALUE : collatz(0, start);
 }
