87fd7b16c58e395093dcd1ba22c02c4442c24a4b
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-17 17:41:17 -0600

    [c/hamming] Update exercise

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,16 +1,15 @@
+#include "hamming.h"
 #include <stddef.h>
 #include <string.h>
-#include "hamming.h"
 
-int compute(const char *lhs, const char *rhs)
-{
-  int c = -1;
-  if (lhs && rhs && (strlen(lhs) == strlen(rhs))) {
-    c = 0;
+int compute(const char *lhs, const char *rhs) {
+    if (!lhs || !rhs || !(strlen(lhs) == strlen(rhs)))
+        return -1;
+
+    int distance = 0;
     int l = (int)strlen(lhs);
     for(int i=0; i<l; i++)
-      if(lhs[i] != rhs[i])
-        c++;
-  }
-  return c;
+        if(lhs[i] != rhs[i])
+            distance++;
+    return distance;
 }
