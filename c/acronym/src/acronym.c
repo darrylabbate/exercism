70a04a182e8740a7eefd0d43df8327c616f48525
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-17 20:22:32 -0600

    [c/acronym] Finish solution

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -2,19 +2,21 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MAXPHRASE 256
+#include <string.h>
 
 char *abbreviate(const char *phrase) {
-    if (phrase == NULL)
+    if (phrase == NULL || strlen(phrase) == 0)
         return NULL;
 
-    char *a = calloc(1,20);
+    char *a = calloc(1, strlen(phrase));
+    int   i = 0;
+
+    a[i++] = toupper(*phrase++);
 
     while (*phrase) {
         switch (*phrase++) {
-        case ' ': sprintf(a++, "%c", toupper(*(++phrase))); break;
-        case '-': sprintf(a++, "%c", toupper(*(++phrase))); break;
+        case ' ': a[i++] = toupper(*phrase++); break;
+        case '-': a[i++] = toupper(*phrase++); break;
         default: break;
         }
     }
