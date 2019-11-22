14b5679201be89d6740054a6a88677fdab836f84
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-21 22:14:12 -0600

    [c/binary] Implement without strlen()

--- c/binary/src/binary.c
+++ c/binary/src/binary.c
@@ -1,13 +1,12 @@
 #include "binary.h"
-#include <string.h>
 
 int convert(const char *input) {
     int x = 0;
-    int e = strlen(input) - 1;
     while (*input) {
+        x <<= 1;
         switch (*input++) {
-        case '0': e--; break;
-        case '1': x += 1 << e--; break;
+        case '1': ++x;
+        case '0': break;
         default: return INVALID;
         }
     }
