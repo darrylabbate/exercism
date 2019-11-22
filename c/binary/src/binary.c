43e58b98bd8ba969a8c5cd5f2125e55e9f1f7031
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-21 20:32:05 -0600

    [c/binary] Implement without pow()

--- c/binary/src/binary.c
+++ c/binary/src/binary.c
@@ -1,16 +1,15 @@
 #include "binary.h"
-#include <math.h>
 #include <string.h>
 
 int convert(const char *input) {
-    int d = 0;
+    int x = 0;
     int e = strlen(input) - 1;
     while (*input) {
         switch (*input++) {
         case '0': e--; break;
-        case '1': d += pow(2, e--); break;
+        case '1': x += 1 << e--; break;
         default: return INVALID;
         }
     }
-    return d;
+    return x;
 }
