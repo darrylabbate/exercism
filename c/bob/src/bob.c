f05beb11ed65071c40a4f8a9fa69c646f9002bb5
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-23 15:54:49 -0600

    [c/bob] Finish solution

--- c/bob/src/bob.c
+++ c/bob/src/bob.c
@@ -1,5 +1,8 @@
 #include "bob.h"
+#include <ctype.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define SILENCE      "Fine. Be that way!"
 #define NORMAL       "Whatever."
@@ -7,17 +10,43 @@
 #define QUESTION     "Sure."
 #define YELLQUESTION "Calm down, I know what I'm doing!"
 
+static char *trim(char *str) {
+    char *x = calloc(1, strlen(str) + 1);
+    int i = 0;
+    while (*str) {
+        switch (*str) {
+        case ' ':
+        case '\n':
+        case '\r':
+        case '\t': str++; break;
+        default:   x[i++] = *str++; break;
+        }
+    }
+    return x;
+}
+
 static bool yell(char *greeting) {
+    for (size_t i = 0; i < strlen(greeting); i++) {
+        if (isupper(greeting[i]))
+            return isupper(greeting[i]) && isupper(greeting[i + 1]);
+    }
+    return false;
 }
 
 static bool question(char *greeting) {
+    return greeting[strlen(greeting) - 1] == '?';
 }
 
 char *hey_bob(char *greeting) {
-    if (yell(greeting) && question(greeting))
+    greeting = trim(greeting);
+    if (!*greeting)
+        return SILENCE;
+    else if (yell(greeting) && question(greeting))
         return YELLQUESTION;
     else if (yell(greeting))
         return YELL;
     else if (question(greeting))
         return QUESTION;
+    else
+        return NORMAL;
 }
