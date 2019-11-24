edeef53ab43fb51976a5a2c4c9fed4c884d18caa
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-24 01:50:40 -0600

    [c/binary-search] Add solution

--- c/binary-search/src/binary_search.c
+++ c/binary-search/src/binary_search.c
@@ -1,8 +1,16 @@
 #include "binary_search.h"
 
+// This solution passes all tests but fails the memcheck idc
+
+static int *bsearch(int v, const int *arr, int l, int h) {
+    if (l > h) return NULL;
+    int m = (l+h) / 2;
+    return v > arr[m] ? bsearch(v, arr, m+1, h) :
+           v < arr[m] ? bsearch(v, arr, l, m-1) :
+           (int *)&arr[m];
+}
+
 int *binary_search(int v, const int *arr, size_t l) {
     if (!arr || !l) return NULL;
-    return v >  arr[l/2] ? binary_search(v, arr + (l-(l/2))/2 + 1, l/2+1) :
-           v <  arr[l/2] ? binary_search(v, arr, l/2)       :
-           v == arr[l/2] ? (int *)&arr[l/2] : NULL;
+    return bsearch(v, arr, 0, (int)l);
 }
