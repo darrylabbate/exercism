6f4466378c20e53247e3fafc974340c23c6c2654
Darryl Abbate <rootbeersoup@gmail.com>
2019-11-24 15:03:16 -0600

    [c/rational-numbers] Add exercise

---
+++ c/rational-numbers/src/rational_numbers.c
@@ -0,0 +1,25 @@
+#include "rational_numbers.h"
+
+rational_t add(rational_t r1, rational_t r2) {
+}
+
+rational_t subtract(rational_t r1, rational_t r2) {
+}
+
+rational_t multiply(rational_t r1, rational_t r2) {
+}
+
+rational_t divide(rational_t r1, rational_t r2) {
+}
+
+rational_t absolute(rational_t r) {
+}
+
+rational_t exp_rational(rational_t r, uint16_t n) {
+}
+
+float exp_real(uint16_t n, rational_t r) {
+}
+
+rational_t reduce(rational_t r) {
+}
